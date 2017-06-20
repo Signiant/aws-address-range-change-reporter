c581dafa0963d64a31ca93b480dae8993e751758
Dave North <dnorth@signiant.com>
2017-06-20 16:23:26 -0400

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # aws-address-range-change-reporter
 Notifies in slack whenever the AWS public IP address range changes
+
+# Running
+If you are running in AWS using a role, you do not need to specify the access/secret keys
+
+```bash
+docker run \
+   -e WEBHOOK_ENDPOINT='https://your_slack_inbound_webhook_url' \
+   -e WEBHOOK_USER='slack_name_for_the_webhook_user_to_post_as'
+   -e SLACK_CHANNEL='#my_slack_channel' \
+   -e S3_PREV_RUN_FILE='s3://mybucket/mykey/address_range_check.prev' \
+   -e AWS_ACCESS_KEY_ID='YOUR_AK' \
+   -e AWS_SECRET_ACCESS_KEY='YOUR SK' \
+   signiant/aws-address-range-change-reporter
+```
+
+where...
+* WEBHOOK_ENDPOINT is as provided by Slack for an inbound webhook
+* WEBHOOK_USER is the name you'd like to show for the "user" making the slack post
+* SLACK_CHANNEL is the slack channel to report the address range diffs to
+* S3_PREV_RUN_FILE is a file in S3 that contains the previous run's values to diff against
---
+++ app/aws-address-range-check.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+INST_DIR=/app
+CURRENT_JSON=${INST_DIR}/current.json
+PREVIOUS_JSON=${INST_DIR}/previous.json
+
+AWS_ADDRESS_URL=https://ip-ranges.amazonaws.com/ip-ranges.json
+
+SLACK_TEMPLATE=${INST_DIR}/slack-template.json
+
+CURL=/usr/bin/curl
+
+if [ -e "${CURRENT_JSON}" ]; then
+	mv ${CURRENT_JSON} ${PREVIOUS_JSON}
+fi
+
+# get the json address list
+$CURL -s ${AWS_ADDRESS_URL} > ${CURRENT_JSON}
+RESULT=$?
+
+if [ $RESULT -eq 0 ]; then
+	if [ -e "${PREVIOUS_JSON}" ]; then
+		# This is not our first run
+		PREVIOUS_MODIFIED_TIME=$(cat ${PREVIOUS_JSON} | jq -r '.["createDate"]')
+		CURRENT_MODIFIED_TIME=$(cat ${CURRENT_JSON} | jq -r '.["createDate"]')
+
+		if [ "${PREVIOUS_MODIFIED_TIME}" != "${CURRENT_MODIFIED_TIME}" ]; then
+			SLACK_MESSAGE_BODY=$(diff ${CURRENT_JSON} ${PREVIOUS_JSON} |grep -e 'ip_prefix' -e 'region' -e 'service' -e 'createDate' -e '{' |sed -e 's/>\s*{//g;s/>/REMOVED/g;s/</ADDED/g;s/"//g;s/,//g' |sed  ':a;N;$!ba;s/\n/\\n/g')
+
+			cat ${SLACK_TEMPLATE} |
+				jq --arg channel ${SLACK_CHANNEL} \
+				   --arg message_body "${SLACK_MESSAGE_BODY}" \
+				   --arg user "${WEBHOOK_USER}" \
+					'.["channel"] = $channel|.["username"] = $user|.["attachments"][0]["fields"][0]["value"] = $message_body' > /tmp/slack1.$$
+
+			cat /tmp/slack1.$$ | sed -e 's/\\n/n/g' > /tmp/slack2.$$
+		fi
+	else
+		SLACK_MESSAGE="Inital run of the AWS address range checker. Baseline taken"
+		echo ${SLACK_MESSAGE}
+	fi
+else
+	SLACK_MESSAGE="ERROR We have been seduced by the dark side and are unable to download the AWS address range json"
+	echo ${SLACK_MESSAGE}
+fi
+
+if [ ! -z "${SLACK_MESSAGE}" ]; then
+	$CURL -s -X POST --data-urlencode "payload={\"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${WEBHOOK_USER}\", \"text\": \"<!channel>  ${SLACK_MESSAGE}\"}" ${WEBHOOK_ENDPOINT}
+elif [ -e /tmp/slack2.$$ ]; then
+	curl -s -X POST --data-urlencode "payload=`cat /tmp/slack2.$$`" ${WEBHOOK_ENDPOINT}
+	rm -f /tmp/slack?.$$
+else
+	echo "no slack message to post"
+fi
